26444baf74b1a7fc5610e90e2a5c8b048edc094b
Allison Bates <allisonkbates@gmail.com>
2016-11-17 18:54:10 -0500

    test

---
+++ newalgorithms.js
@@ -0,0 +1,22 @@
+function diffArray(arr1, arr2) {
+	for (var i =0; i<arr1.length; i++) {
+		var index = arr1[i].indexOf(arr2[i]);
+		var index1 = [];
+		index1.push();
+	}
+return index1;
+
+	// loop through first array
+	//find index of each value in 2nd array
+	//take those (use filter) that are not indexable and put them in their own array - other array
+	//
+	//loop through 2nd array
+	//find index of value in first array
+	//take those that are not indexable and add them to other array
+
+
+
+
+}
+
+diffArray([1,2,3,4,5], [1,2,3,6]);
