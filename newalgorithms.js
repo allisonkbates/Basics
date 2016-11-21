4b4ec90ff6fcab81e20aa721abffe7e4bc68fcbd
Allison Bates <allisonkbates@gmail.com>
2016-11-21 14:33:44 -0500

    diffArray solution

--- newalgorithms.js
+++ newalgorithms.js
@@ -1,17 +1,44 @@
 function diffArray(arr1, arr2) {
- 
- arr1.forEach(function(indexes){
-	return arr2.indexOf(indexes);
-             });
+var indexnone1 = [];
+var indexnone2 = [];
+for (var i =0; i<arr1.length; i++) {
+	var index1 = arr2.indexOf(arr1[i]);
+		if (index1 === -1) {
+			indexnone1.push(arr1[i]);
+	  	} else {
+			indexnone2.push(index1);
+	  	}
+}
+for (i=0; i <arr2.length; i++) {
+	var index2 = arr1.indexOf(arr2[i]);
+    	if (index2 === -1) {
+       		indexnone1.push(arr2[i]);
+     	} else {
+       		indexnone2.push(index2);
+   		}
+}
+  
+return indexnone1;
 
+}
+
+diffArray([1,2,3,4,5], [1,2,3,6]);
+
+
+	/*function getIndex1(item, index) {
+		console.log(item + index);
+	}
 
 
  /*	var indexnone1 = [];
  	var indexnone2 = [];
 	for (var i =0; i<arr1.length; i++) {
 		var index1 = arr2.indexOf(arr1[i]);
-		indexnone1.push(index1);
-	}
+		if (index1 === -1) {
+			indexnone1.push(index1);
+		} else {
+		indexnone2.push(index1);
+	}}
 
 	for (var j=0; j<arr2.length; j++) {
 		var index2 = arr1.indexOf(arr2[i]);
@@ -28,10 +55,3 @@ function diffArray(arr1, arr2) {
 	//find index of value in first array
 	//take those that are not indexable and add them to other array
 	
-
-
-
-
-}
-
-diffArray([1,2,3,4,5], [1,2,3,6]);
