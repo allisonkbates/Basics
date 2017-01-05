fd05d6b5b79b652d5320e7a9a09266b0c21c96c5
Allison Bates <allisonkbates@gmail.com>
2017-01-04 22:14:05 -0500

    continued work

---
+++ smallestcommonmultiple.js
@@ -0,0 +1,28 @@
+function smallestCommons(arr) {
+
+//first for loop, goes through each multiple of the greatest number
+//second for loop, checks if the multiple of the greatest number is compatible with remaining lower numbers
+
+var max = Math.max(arr[0], arr[1]);
+var maxminus = max-1;
+
+
+
+
+
+
+
+/*var num;
+var max = Math.max(arr[0], arr[1]);
+var maxminus = max-1;
+for (var i = max; ; i+max) {
+  for (var j=maxminus; i%j===0; j--) {
+  if (i%j===0) {
+    num = i;
+  }
+  }
+     
+}  return num;
+  */
+}  
+smallestCommons([1,5]);
