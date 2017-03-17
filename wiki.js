412f99001070d3e2ab49e983f5a77e7428e51cf3
Allison Bates <allisonkbates@gmail.com>
2017-03-17 10:35:08 -0400

    updated with map

--- wiki.js
+++ wiki.js
@@ -9,10 +9,55 @@ window.onload = function () {
   }
 
   function getData() {
-    $.getJSON(getUrl(), function(data) {
-        console.table(data.query.search);
-        var title = data.query.search[0].title;
-        document.getElementById("title").innerHTML = title;
+    $.getJSON(getUrl(), function (data) {
+        var queryArray = data.query.search;
+        var titleArray = [];
+        var snippetArray = [];
+        
+        console.log(queryArray);
+
+        queryArray.map(function (query) {
+          var results = document.getElementById('results'); //results
+          var li = document.createElement('li');
+          var textNode = document.createTextNode(query.title);
+          li.appendChild(textNode);
+          results.appendChild(li);
+          var ul = document.createElement('ul');
+          var li2 = document.createElement('div');
+          var div2 = li2.write(query.snippet);
+          results.appendChild(li2);
+          /*
+          var textNode2 = document.createTextNode(query.snippet.;
+          li2.appendChild(textNode2);
+          results.appendChild(li2);*/
+
+        });
+
+        /*queryArray.forEach(function (query) {
+          titleArray.push(query.title);
+          snippetArray.push(query.snippet);
+        });
+
+        titleArray.forEach(function (title) {
+          var results = document.getElementById('results'); //results
+          var li = document.createElement('li');
+          var textNode = document.createTextNode(title);
+          li.appendChild(textNode);
+          results.appendChild(li);
+
+
+
+        });
+         snippetArray.forEach(function (snippet) {
+          var ul = document.createElement('ul');
+          results.appendChild(ul);
+          var li2 = document.createElement('li');
+          var snippetNode = document.createTextNode(snippet);
+          li2.appendChild(snippetNode);
+          
+          ul.appendChild(li2);
+        });   */ 
+
     })
       .done(function() {
         console.log("data loaded successfully!");
@@ -27,7 +72,11 @@ window.onload = function () {
     getData();
   }
 
+  function showResults() {
+
+  }
+
   search.addEventListener("submit", searchWiki);
-
-
 }
+
+//figure out how to get fa-search into the html via js??
